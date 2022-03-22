b97c170b81391259ecb128c022acb00dbae2b545
AlexaEgorova <egorova.a007@gmail.com>
2022-03-22 22:03:00 +0300

    some kind of result module_2_task_1

--- cv_book/Module_II/task_1.py
+++ cv_book/Module_II/task_1.py
@@ -1,25 +1,57 @@
 #Уменьшить засветку от света фар встречного автомобиля [klt.427.003]
 
 import numpy as np
-
 import cv2
 
-def reduce_car_backlights(img: np.array):
-    gamma = 0.8
+
+def gamma_correction(img: np.array, gamma: float):
     look_up_table = np.empty((1,256), np.uint8)
     for i in range(256):
         look_up_table[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
     res = cv2.LUT(img, look_up_table)
-
     return res
+
+
+def get_overexposed_mask(gray_img: np.array, threshold: int, need_blur: bool = True):
+    gray = gray_img.copy()
+    if need_blur:
+        gray = cv2.GaussianBlur(gray_img, (11, 11), 0)
+    return cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)[1]
+
+
+def get_overexposed_mask_by_channels(img: np.array, threshold: int, need_blur: bool = True):
+    b, g, r = cv2.split(img)
+    masks = [get_overexposed_mask(channel, threshold, need_blur) for channel in [b, g, r]]
+    return sum(masks)
+
+
+def get_masked(img: np.array, mask: np.array):
+    return cv2.bitwise_and(img, img, mask=mask)
+
+
+def apply_masked_changes(img: np.array, changed_image: np.array, mask: np.array):
+    img[np.where(mask == 255)] = changed_image[np.where(mask == 255)]
+    return img
+
+
+def reduce_light_struck(img: np.array):
+    cv2.imshow("img", img)
+    img = gamma_correction(img, 1.1)
+    res = img.copy()
+    gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    for th in range(250, 50, -10):
+        overexposed_mask = get_overexposed_mask(gray, th, need_blur=True)
+        overexposed = get_masked(res, overexposed_mask)
+        reduced_overexposure = gamma_correction(overexposed, 1.025)
+        res = apply_masked_changes(res, reduced_overexposure, overexposed_mask)
+
+    cv2.imshow('res', res)
 
 
 if __name__ == '__main__':
     img = cv2.imread('test_1.png')
-    img = cv2.resize(img, (640, 360), interpolation=cv2.INTER_AREA)
-    img_remastered = reduce_car_backlights(img)
+    img = cv2.resize(img, (650, 350), interpolation=cv2.INTER_AREA)
 
-    cv2.imshow('lights', img)
-    cv2.imshow('no_lights', img_remastered)
+    reduce_light_struck(img)
 
     cv2.waitKey(0)

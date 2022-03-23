73222c60ecd915ae201988e298e10f4b993506e4
AlexaEgorova <egorova.a007@gmail.com>
2022-03-23 19:49:16 +0300

    fixed img types, added func bez pontov

--- cv_book/Module_II/task_1.py
+++ cv_book/Module_II/task_1.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 
 
-def gamma_correction(img: np.array, gamma: float):
+def gamma_correction(img: np.ndarray, gamma: float):
     look_up_table = np.empty((1,256), np.uint8)
     for i in range(256):
         look_up_table[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
@@ -12,7 +12,7 @@ def gamma_correction(img: np.array, gamma: float):
     return res
 
 
-def get_overexposed_mask(gray_img: np.array, threshold: int, need_blur: bool = True):
+def get_overexposed_mask(gray_img: np.ndarray, threshold: int, need_blur: bool = True):
     """
     Получить маску засвеченных областей с заданным порогом
     на основе перехода к grayscale
@@ -23,7 +23,7 @@ def get_overexposed_mask(gray_img: np.array, threshold: int, need_blur: bool = T
     return cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)[1]
 
 
-def get_overexposed_mask_by_channels(img: np.array, threshold: int, need_blur: bool = True):
+def get_overexposed_mask_by_channels(img: np.ndarray, threshold: int, need_blur: bool = True):
     """
         Получить маску засвеченных областей с заданным порогом
         на основе разложения на каналы
@@ -33,18 +33,20 @@ def get_overexposed_mask_by_channels(img: np.array, threshold: int, need_blur: b
     return sum(masks)
 
 
-def get_masked(img: np.array, mask: np.array):
+def get_masked(img: np.ndarray, mask: np.ndarray):
     """Получить фрагменты изображения под маской"""
     return cv2.bitwise_and(img, img, mask=mask)
 
 
-def apply_masked_changes(img: np.array, changed_image: np.array, mask: np.array):
+def apply_masked_changes(img: np.ndarray,
+                         changed_image: np.ndarray,
+                         mask: np.ndarray):
     """Заменить в img маскированные области на области из changed_image"""
     img[np.where(mask == 255)] = changed_image[np.where(mask == 255)]
     return img
 
 
-def reduce_light_struck(img: np.array):
+def reduce_light_struck(img: np.ndarray):
     cv2.imshow("img", img)
     img = gamma_correction(img, 1.1)
     res = img.copy()
@@ -58,10 +60,18 @@ def reduce_light_struck(img: np.array):
     cv2.imshow('res', res)
 
 
+def reduce_light_struck_bez_pontov(img: np.ndarray):
+    res = gamma_correction(img, 1.4)
+    cv2.imshow('res', res)
+    return res
+
+
 if __name__ == '__main__':
     img = cv2.imread('test_1.png')
     img = cv2.resize(img, (650, 350), interpolation=cv2.INTER_AREA)
 
     reduce_light_struck(img)
+    reduce_light_struck_bez_pontov(img)
+
 
     cv2.waitKey(0)
